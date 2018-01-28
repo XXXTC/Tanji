06d58cdedb2c6a8ca72e2a006ccc07d586ae3b82
ArachisH <iKBenPeanut@gmail.com>
2018-01-27 23:21:06 -0800

    The test worked, hi discord

--- Tanji/Program.cs
+++ Tanji/Program.cs
@@ -43,7 +43,6 @@ namespace Tanji
         [STAThread]
         public static void Main()
         {
-            // This is test..
             Eavesdropper.Certifier = new CertificateManager("Tanji", "Tanji Certificate Authority");
             Eavesdropper.Overrides.AddRange(new[]
             {
